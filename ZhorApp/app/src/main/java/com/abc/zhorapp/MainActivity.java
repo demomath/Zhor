673a750eca42a0e5b8a7fec5188baaabb8848bf2
wudi <wudi@wudideMacBook-Air.local>
2018-04-23 15:56:51 +0800

    fix app and all file

---
+++ ZhorApp/app/src/main/java/com/abc/zhorapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.abc.zhorapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
