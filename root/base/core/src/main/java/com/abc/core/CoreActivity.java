4cc112ac6e6301539dcb2542474433ff302e1d13
wudi <wudi@wudideMacBook-Air.local>
2018-04-24 17:19:47 +0800

    fix tree

---
+++ root/base/core/src/main/java/com/abc/core/CoreActivity.java
@@ -0,0 +1,14 @@
+package com.abc.core;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoreActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_core);
+    }
+}
---
+++ zhor/app/src/main/java/com/abc/zhorapp/ZhorApplication.java
@@ -0,0 +1,12 @@
+package com.abc.zhorapp;
+
+
+import com.abc.common.CommonApplication;
+
+/**
+ * Created by wudi on 2018/4/23.
+ */
+
+public class ZhorApplication extends CommonApplication {
+
+}
